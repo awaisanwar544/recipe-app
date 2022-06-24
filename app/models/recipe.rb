25e8a4b77811c7cf7401bc9aec844146cf45346d
awaisanwar544 <awaisanwar544@gmail.com>
2022-06-25 00:29:03 +0500

    Add ingredients for Recipe

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,5 +1,7 @@
 class Recipe < ApplicationRecord
   belongs_to :user
+  has_many :recipe_foods, dependent: :destroy
+  has_many :foods, through: :recipe_foods
 
   validates :name, presence: true, uniqueness: { scope: :user_id }
   validates :description, presence: true
